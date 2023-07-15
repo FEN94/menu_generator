12077dcd12a4823b7de3ac6919eee89e945145d8
Fernando Estevez <fernandoestevez8494@gmail.com>
2023-07-15 14:11:16 -0400

    files redesinged

--- Articles.dart
+++ Articles.dart
@@ -1,23 +1,22 @@
-import 'Article.dart';
 import 'dart:math';
 
 class Articles {
   String _category_name = "";
-  final _article_list = <Article>[];
+  final _article_list = <String>[];
 
   Articles(String category_name) {
     this._category_name = category_name;
   }
 
-  void add_article(Article article) {
+  void add_article(String article) {
     this._article_list.add(article);
   }
 
-  List<Article> get_articleList() {
+  List<String> get_articleList() {
     return this._article_list;
   }
 
-  List<Articles> randomized_list(int no_articles) {
+  List<String> randomized_list(int no_articles) {
     int index_random = Random().nextInt(no_articles);
     return List.empty();
   }
--- main.dart
+++ main.dart
@@ -1,8 +1,45 @@
 import 'dart:async';
 import 'dart:io';
+import './Articles.dart';
+
+enum Day { Lunes, Martes, Miercoles, Jueves, Viernes, Sabado }
 
 void main(List<String> args) {
-  File("./Articles.txt").readAsString().then((String content) {
-    print(content);
-  });
+  String output = "";
+  List<Articles> articles = List<Articles>.empty();
+  String file_content = read_article_file();
+  articles = create_Articles(file_content);
+
+  return;
+}
+
+String read_article_file() {
+  File file = File("./Articles.txt");
+  String content = file.readAsStringSync();
+  return content;
+}
+
+List<Articles> create_Articles(String content) {
+  List<String> content_splitted = content.split("==");
+  List<Articles> output_list = List.empty(growable: true);
+
+  for (var i = 0; i < content_splitted.length; i++) {
+    List<String> category_content = content_splitted[i].split('\n');
+    Articles articles = Articles("");
+    for (var i = 0; i < category_content.length; i++) {
+      if (category_content[i].contains('-')) {
+        articles = Articles(category_content
+            .elementAt(i)
+            .replaceAll("-", "")
+            .replaceAll(":", ""));
+      } else {
+        if (!category_content.elementAt(i).isEmpty) {
+          articles
+              .add_article(category_content.elementAt(i).replaceAll("+", ""));
+        }
+      }
+    }
+    output_list.add(articles);
+  }
+  return output_list;
 }
