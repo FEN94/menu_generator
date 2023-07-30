85ba065264d7a440d1a60cbc3e19d0b9c295ebd5
Fernando Estevez <fernandoestevez8494@gmail.com>
2023-07-30 16:11:17 -0400

    added new fuction _set_primaryArticle()

--- Articles.dart
+++ Articles.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 class Articles {
   String _category_name = "";
   final _article_list = <String>[];
+  final _priority_articles = <String>[];
 
   Articles(String category_name) {
     this._category_name = category_name;
@@ -10,6 +11,10 @@ class Articles {
 
   void add_article(String article) {
     this._article_list.add(article);
+  }
+
+  void add_priority_article(String article) {
+    this._priority_articles.add(article);
   }
 
   List<String> get_articleList() {
@@ -23,39 +28,43 @@ class Articles {
   List<String> randomized_list(int no_articles) {
     List<String> articles = List.empty(growable: true);
     int flag = 0;
-    // bool beef = false, pork = false;
+    bool beef = false, pork = false;
+
+    articles = _set_primaryArticle(this._category_name, articles);
+    flag = this._priority_articles.length;
     while (flag < no_articles) {
       int index_random = Random().nextInt(this._article_list.length);
       String article = this._article_list.elementAt(index_random);
 
-      if ((article.contains("Bistec") || article.contains("Cerdo")) &&
-          this._category_name == "Acompañantes" &&
-          articles.length > 0) {
-        bool flag2 = false;
-        for (var i = 0; i < articles.length; i++) {
-          if (articles[i].contains("Bistec") || articles[i].contains("Cerdo")) {
-            flag2 = true;
-            break;
-          }
+      if (this._category_name == "Acompañantes") {
+        if (!beef && article.contains("Bistec")) {
+          articles.add(article);
+          flag++;
+          beef = !beef;
+          continue;
         }
-        if (flag2) {
-          flag2 = false;
+        if (!pork && article.contains("Cerdo")) {
+          articles.add(article);
+          flag++;
+          pork = !pork;
           continue;
         }
       }
 
-      // if (this._category_name.contains("Acompañantes")) {
-      //   if (!beef && article.contains("Bistec")) {
-      //     !beef;
-      //   }
-      //   if (!pork && article.contains("Cerdo")) {
-      //     !pork;
-      //   }
-      // }
-
-      if (!articles.contains(article)) {
+      if (!articles.contains(article) &&
+          !article.contains("Bistec") &&
+          !article.contains("Cerdo")) {
         articles.add(article);
         flag++;
+      }
+    }
+    return articles;
+  }
+
+  List<String> _set_primaryArticle(String category, List<String> articles) {
+    if (this._priority_articles.length > 0) {
+      for (var article in this._priority_articles) {
+        articles.add(article);
       }
     }
     return articles;
